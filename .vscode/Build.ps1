bdf3085240588fca3f38b5b82353433dc20c6134
Marc Kuhn <marc.kuhn@mykuhn.net>
2024-06-29 20:29:40 +0200

    Corrected errors

--- .vscode/Build.ps1
+++ .vscode/Build.ps1
@@ -1,5 +1,4 @@
 # Vars
-
 . ".\.vscode\Global.ps1"
 
 # intunewin
@@ -7,19 +6,19 @@
 [string]$Exe = "IntuneWinAppUtil.exe"
 
 # Source content prep tool
-if (-not(Test-Path -Path "$env:ProgramData\$exe")){
+if (-not(Test-Path -Path "$env:ProgramData\$Exe")){
 	Invoke-WebRequest -Uri "$Uri/$Exe" -OutFile "$env:ProgramData\$Exe"
 }
 
 # Execute content prep tool
 $processOptions = @{
 	FilePath = "$env:ProgramData\$Exe"
-	ArgumentList = "-c ""$Cache"" -s ""$Cache\Deploy-Application.exe"" -o ""C:\temp"" -q"
+	ArgumentList = "-c ""$Cache"" -s ""$Cache\Deploy-Application.exe"" -o ""$env:TEMP"" -q"
 	WindowStyle = "Maximized"
 	Wait = $true
 }
-Start-Process @processOptions
+Start-Process @processOptions -Verbose
 
 # Rename and prepare for upload
-Move-Item -Path 'C:\temp\DeployApplication.intunewin' -Destination "$Desktop\$Application.intunewin" -Force -Verbose
+Move-Item -Path "$env:TEMP\Deploy-Application.intunewin" -Destination "$Desktop\$Application.intunewin" -Force -Verbose
 explorer $Desktop
