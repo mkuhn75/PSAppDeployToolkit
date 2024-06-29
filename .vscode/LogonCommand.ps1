6f8105504cf98e1f7aa77a63a7b04a79665a8a6a
Marc Kuhn <marc.kuhn@mykuhn.net>
2024-06-29 22:08:52 +0200

    Copied Template Files

---
+++ .vscode/LogonCommand.ps1
@@ -0,0 +1,17 @@
+Write-Host 'Testing has started...' -ForegroundColor Cyan
+Start-Process -FilePath 'C:\Users\WDAGUtilityAccount\Desktop\master\Deploy-Application.exe' -Wait
+Write-Host 'Installation completed' -ForegroundColor DarkGreen
+Write-Host 'you have 60 seconds to verify the installation before it is automatically uninstalled' -ForegroundColor Cyan
+
+$Seconds = 60
+$EndTime = [datetime]::UtcNow.AddSeconds($Seconds)
+
+while (($TimeRemaining = ($EndTime - [datetime]::UtcNow)) -gt 0) {
+	Write-Progress -Activity 'Waiting for...' -Status testing -SecondsRemaining $TimeRemaining.TotalSeconds
+	Start-Sleep 1
+}
+
+Start-Process -FilePath 'C:\Users\WDAGUtilityAccount\Desktop\master\Deploy-Application.exe' -ArgumentList 'Uninstall' -Wait
+Write-Host 'test completed' -ForegroundColor DarkGreen
+Write-Host 'You can close sandbox now!' -ForegroundColor Cyan
+Read-Host -Prompt 'Press any key to continue...'
--- .vscode/Sandbox.ps1
+++ .vscode/Sandbox.ps1
@@ -1,7 +1,7 @@
 # Vars
-. ".\.vscode\Global.ps1"
+. '.vscode\Global.ps1'
 
-#Copy Resources
+# Copy Resources
 Copy-Item -Path ".vscode\$LogonCommand" -Destination "$Win32App\" -Recurse -Force -Verbose -ErrorAction Ignore
 
 # Prepare Sandbox
@@ -9,17 +9,17 @@ Copy-Item -Path ".vscode\$LogonCommand" -Destination "$Win32App\" -Recurse -Forc
 <Configuration>
 <Networking>Enabled</Networking>
 <MappedFolders>
-	<MappedFolder>
-	<HostFolder>$Win32App</HostFolder>
-	<SandboxFolder>$WDADesktop</SandboxFolder>
-	<ReadOnly>true</ReadOnly>
-	</MappedFolder>
+    <MappedFolder>
+    <HostFolder>$Win32App</HostFolder>
+    <SandboxFolder>$WDADesktop</SandboxFolder>
+    <ReadOnly>true</ReadOnly>
+    </MappedFolder>
 </MappedFolders>
 <LogonCommand>
-	<Command>powershell -executionpolicy unrestricted -command "Start-Process powershell -ArgumentList ""-nologo -file $WDADesktop"
+    <Command>powershell -executionpolicy unrestricted -command "Start-Process powershell -ArgumentList ""-nologo -file $WDADesktop\$LogonCommand"""</Command>
 </LogonCommand>
 </Configuration>
 "@ | Out-File "$Win32App\$Application.wsb"
 
 # Execute Sandbox
-Start-Process explorer -ArgumentList "$Win32App\$Application.wsb" -Verbose -WindowStyle Maximized
+Start-Process explorer -ArgumentList "$Win32App\$Application.wsb" -Verbose
